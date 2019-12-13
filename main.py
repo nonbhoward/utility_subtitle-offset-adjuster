0dba3e249d1aad54348d59e7a58a88499d3dabae
Bran Howard <dbrannonhoward@gmail.com>
2019-12-12 22:56:18 -0500

    updated meta-code notes

--- main.py
+++ main.py
@@ -44,10 +44,10 @@ def read_file_contents_by_line(loaded_file, line_number):
 
 
 def read_file_contents_by_line_range(loaded_file, line_start, line_end):
-    for _ in range(line_start, line_end):
-        loaded_file_line_range = loaded_file.readlines(_)
-        # fixme
-    return loaded_file_line_range
+    print("fixme") # fixme
+    # for _ in range(line_start, line_end):
+        # loaded_file_line_range.append(loaded_file.readlines(_))
+    # return loaded_file_line_range
 
 
 def read_line_from_sub_file(sub_file: str, line_number: int):
@@ -55,26 +55,28 @@ def read_line_from_sub_file(sub_file: str, line_number: int):
 
 
 def set_subtitle_extension(file_extension):
-    return file_extension
+    return file_extension  # fixme this makes no sense
 
 
 def set_subtitle_dir(dir_path: str):
     os.chdir(dir_path)
 
 
-# meta-code & notes
-# set/get a working directory @get_subtitle_dir()
-# get a list of all files @find_subtitle_file()
-# filter the list to the relevant subtitle files ".srt"
+# meta-code notes
+# dev-complete : set/get a working directory @get_subtitle_dir()
+# dev-future : browse-able directory selector?
+# dev-complete : get a list of all files in active dir @find_subtitle_file()
+# dev-complete : filter the list to the relevant subtitle files ".srt"
+# dev-future : handle dirs with more than one subtitle file
+# dev-complete : print all contents from the selected text file
+# dev-complete : print a selected line number from the selected text file
+# dev-in-progress : print a selected line range from the selected text file
+# dev-future : present a selectable input if more than one available file
+# dev-future : present the option to shift subtitle forward or back in time
+# dev-in-progress : present the option to input an integer time shift
+# dev-future : present the option to input a decimal time shift
+
 subtitle_filename = find_subtitle_file()
 print("The subtitle file being operated on is : " + str(subtitle_filename))
-# dev : print all contents from the selected text file
 load_subtitle_file(subtitle_filename)
-# dev : print a selected line number from the selected text file
-
-# option : present a selectable input if more than one available file
-# option : present the option to shift subtitle forward or back in time
-# present the option to input an integer time shift
-# option : present the option to input a decimal time shift
-
 # end
