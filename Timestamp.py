d349d3d17a6dac834a70a8462378c915df43c38a
Bran Howard <dbrannonhoward@gmail.com>
2019-12-19 23:49:55 -0500

    created Timestamp class, may or may not use

---
+++ Timestamp.py
@@ -0,0 +1,11 @@
+class Timestamp:
+    def __init__(self, format_string: str):
+        self.format_string = format_string
+
+    def get_format(self):
+        return self.format_string
+
+    def set_format(self, new_format_string):
+        self.format_string = new_format_string
+
+# i may use this, i may not
